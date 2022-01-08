2c5bf2d2c9044deb2e1a4f2ef9573748f4a8f48c
WilmerRS <estwilmerrodriguez@gmail.com>
2022-01-08 07:55:41 -0500

    🗑️fix: removal of pictures when a pet is removed

--- app/Http/Controllers/Pet/DeletePetController.php
+++ app/Http/Controllers/Pet/DeletePetController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Pet;
 use App\Http\Controllers\Controller;
 use App\Http\Response\ResponseFactory;
 use App\Models\Pet;
+use App\Models\Picture;
+use Illuminate\Support\Facades\Storage;
 
 class DeletePetController extends Controller
 {
@@ -18,6 +20,13 @@ class DeletePetController extends Controller
         404
       );
     }
+
+    $pictures = Picture::where('pet_id', $pet->id)->get();
+    foreach ($pictures as $picture) {
+      Storage::delete($picture->path);
+      $picture->delete();
+    }
+
     $pet->delete();
     return ResponseFactory::create(
       'Pet deleted successfully.',
