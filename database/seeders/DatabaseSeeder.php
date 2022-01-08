de051f0c4a90528c67286ed6f4e1a4a6038fa39c
WilmerRS <estwilmerrodriguez@gmail.com>
2022-01-07 19:50:00 -0500

    ⛏️feat: database seeder to project

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+  /**
+   * Seed the application's database.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    $this->call(RolesTableSeeder::class);
+    $this->call(UsersTableSeeder::class);
+    $this->call(CategoriesTableSeeder::class);
+    $this->call(StatusesTableSeeder::class);
+    $this->call(PetsTableSeeder::class);
+  }
+}
